4a18a732cfc2df794e760db2b32ce12bc6dad4fa
Marc Joliet <marcec@gmx.de>
2010-04-28 14:03:22 +0000

    compile_msndfile.m: add support for Linux and Mac.
    As an easy to use alternative to the full-fledged SCons build system, there is
    now an m-File that compiles via the mex command. While it is much easier to use
    and doesn't require additional software, it doesn't offer full control over the
    system toolchain, which is the goal of the SCons build system.

--- compile_msndfile.m
+++ compile_msndfile.m
@@ -1,1 +1,9 @@
-mex -LWin -l'sndfile-1' -Iinclude -IWin msndfile.c
+% Simple script for compiling msndfile.
+
+if strcmp(computer, 'PCWIN') || strcmp(computer, 'PCWIN64') 
+    mex -LWin -l'sndfile-1' -Iinclude -IWin msndfile.c
+elseif strcmp(computer, 'GLNX86') || strcmp(computer, 'GLNXA64') 
+    mex -LLinux -lsndfile -Iinclude msndfile.c
+elseif strcmp(computer, 'MACI') || strcmp(computer, 'MACI')
+    mex -LMac -lsndfile -Iinclude msndfile.c
+end
