793aa6334621382c23fbabc2381ca56eab4dde76
Marc Joliet <marcec@gmx.de>
2011-08-23 22:29:26 +0200

    msndfile: clean up code, transpose audio data.
    I Added several comments in the code and tidied up in some places. Also, I
    (arbitrarily) limited the number of channels to 12 in order to prevent
    accidental overfilling of RAM and therefor swapping.  I also modified msndfile
    to return arrays in the form NrOfSamples x NrOfChannels (instead of the other
    way around).  For that, msndfile transposes the data array returned by
    sf_readf_double().

--- msndfile.c
+++ msndfile.c
@@ -1,9 +1,16 @@
+#include <string.h>
 #include <mex.h>
 #include <sndfile.h>
-#include <string.h>
+
+/*
+ * This is a simple mex-File using libsndfile for reading in audio files
+ *
+ * TODO: this needs more testing
+ */
 
 SNDFILE* sf_input_file;
 
+/* function for clearing memory after this program ends */
 void clear_memory(void)
 {
     if( !sf_input_file )
@@ -13,68 +20,71 @@ void clear_memory(void)
 void mexFunction(int nlhs, mxArray *plhs[],
                  int nrhs, const mxArray *prhs[])
 {
-    int i; // counter in for-loops
-    int str_size;
-    char* sf_input_fname;
-    int num_frames;
-    double* data;
-    int processed_frames;
-    int sndfile_error;
-    double* fs;
-    SF_INFO* sf_file_info;
+    int         i; // counter in for-loops
+    int         sndfile_error;
+    int         num_channels; // temporary hack for Gentoo
+    const int   str_size = mxGetN(prhs[0])+1;
+    char        *sf_input_fname;
+    sf_count_t  num_frames, processed_frames;
+    double      *data, *output, *fs;
+    SF_INFO     *sf_file_info;
 
     mexAtExit(&clear_memory);
 
     if( nrhs < 1 )
         mexErrMsgTxt("Missing argument: you need to at least pass an array or a file name.");
 
-    /* all this just to get to a string... */
-    str_size = mxGetN(prhs[0])+1;
+    /* get input filename string */
     sf_input_fname = (char*)mxCalloc(str_size, sizeof(char));
-    if( sf_input_fname == NULL )
+    if( !sf_input_fname )
         mexErrMsgTxt("mxCalloc error!");
     mxGetString(prhs[0], sf_input_fname, str_size);
 
     /* initiate sf_file_info struct pointer */
     sf_file_info = (SF_INFO*)mxMalloc(sizeof(SF_INFO));
     if( !sf_file_info )
-        mexErrMsgTxt("could not allocate SF_INFO* instance");
-    /* format needs to be set to 0 before a file is opened */
+        mexErrMsgTxt("Could not allocate SF_INFO* instance");
+    /* "format" needs to be set to 0 before a file is opened for reading */
     sf_file_info->format = 0;
 
     /* open sound file */
     mexPrintf("Opening file...\n");
     sf_input_file = sf_open(sf_input_fname, SFM_READ, sf_file_info);
     if( !sf_input_file )
-        mexErrMsgTxt("could not open sound file");
+        mexErrMsgTxt("Could not open audio file.");
     mexPrintf("Opened file.\n");
 
-    num_frames = (int)sf_file_info->frames;
+    num_frames = sf_file_info->frames;
 
-    /*
-     * set channels to 2 due to a bug in Gentoos 32 bit libsndfile
-     */
-    plhs[0]      = mxCreateDoubleMatrix(sf_file_info->channels, (int)sf_file_info->frames, mxREAL);
-    /* plhs[0]      = mxCreateDoubleMatrix(2, (int)sf_file_info->frames, mxREAL); */
-    data = mxGetPr(plhs[0]);
+    /* set channels to 2 due to a bug in Gentoos 32 bit libsndfile */
+    /* num_channels = 2; */
+    num_channels = sf_file_info->channels;
+    plhs[0]      = mxCreateDoubleMatrix((int)sf_file_info->frames, num_channels, mxREAL);
+    output       = mxGetPr(plhs[0]);
+    data         = (double*)mxCalloc((int)sf_file_info->frames*num_channels,sizeof(double));
 
-    /* TODO: need to correct/test this */
+    /* read the entire file in one go */
+    processed_frames = sf_readf_double(sf_input_file, data, num_frames);
 
-    /* read in the entire file */
-    processed_frames = (int)sf_readf_double(sf_input_file, data, num_frames);
+    /* transpose returned data */
+    for( i=0; i<num_frames; i+=num_channels )
+    {
+        int j;
+        for( j=0; j<num_channels; j++ )
+            output[i+j*num_frames] = data[i*num_channels+j];
+    }
 
-    if( processed_frames < num_frames && sf_file_info->seekable )
-        processed_frames = (int)sf_seek(sf_input_file, processed_frames, SEEK_CUR);
-
+    /* rudimentary way of dealing with libsndfile errors */
     sndfile_error = sf_error(sf_input_file);
     if( sndfile_error != SF_ERR_NO_ERROR ) {
         mexWarnMsgTxt("libsndfile error!");
         mexErrMsgTxt(sf_error_number(sndfile_error));
     }
 
+    /* return sampling rate if requested */
     if( nlhs > 1 ) {
-        plhs[1]    = mxCreateDoubleMatrix(1, 1, mxREAL);
-        fs = mxGetPr(plhs[1]);
-        *fs = (double)sf_file_info->samplerate;
+        plhs[1] = mxCreateDoubleMatrix(1, 1, mxREAL);
+        fs      = mxGetPr(plhs[1]);
+        *fs     = (double)sf_file_info->samplerate;
     }
 }
