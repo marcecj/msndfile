226ed5f451117666a81957fda26f0cf798f87714
Marc Joliet <marcec@gmx.de>
2011-09-01 20:14:34 +0200

    Fix compile_msndfile.
    I never use compile_msndfile myself, so I forgot to update it again.

--- compile_msndfile.m
+++ compile_msndfile.m
@@ -1,9 +1,11 @@
 % Simple script for compiling msndfile.
 
-if strcmp(computer, 'PCWIN') || strcmp(computer, 'PCWIN64') 
-    mex -LWin -l'sndfile-1' -Iinclude -IWin msndfile.c
-elseif strcmp(computer, 'GLNX86') || strcmp(computer, 'GLNXA64') 
-    mex -LLinux -lsndfile -Iinclude msndfile.c
+if strcmp(computer, 'PCWIN') || strcmp(computer, 'PCWIN64')
+    mex -LWin -l'sndfile-1' -Iinclude -IWin src/msndfile.c -outdir 'build'
+elseif strcmp(computer, 'GLNX86') || strcmp(computer, 'GLNXA64')
+    mex -LLinux -lsndfile -Iinclude src/msndfile.c -outdir 'build' ...
+        CFLAGS='\$CFLAGS -std=c99'
 elseif strcmp(computer, 'MACI') || strcmp(computer, 'MACI')
-    mex -LMac -lsndfile -Iinclude msndfile.c
+    mex -LMac -lsndfile -Iinclude src/msndfile.c -outdir 'build' ...
+        CFLAGS='\$CFLAGS -std=c99'
 end
