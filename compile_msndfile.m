4f7a4da04a32cfdb7ec4b845d6fb52a9659d111f
Marc Joliet <marcec@gmx.de>
2011-09-03 17:41:29 +0200

    compile_msndfile: add msndblockread compile target.

--- compile_msndfile.m
+++ compile_msndfile.m
@@ -2,10 +2,15 @@
 
 if strcmp(computer, 'PCWIN') || strcmp(computer, 'PCWIN64')
     mex -LWin -l'sndfile-1' -Iinclude -IWin src/msndread.c src/utils.c -outdir 'build'
+    mex -LWin -l'sndfile-1' -Iinclude -IWin src/msndblockread.c src/utils.c -outdir 'build'
 elseif strcmp(computer, 'GLNX86') || strcmp(computer, 'GLNXA64')
     mex -LLinux -lsndfile -Iinclude src/msndread.c src/utils.c -outdir 'build' ...
         CFLAGS='\$CFLAGS -std=c99'
+    mex -LLinux -lsndfile -Iinclude src/msndblockread.c src/utils.c -outdir 'build' ...
+        CFLAGS='\$CFLAGS -std=c99'
 elseif strcmp(computer, 'MACI') || strcmp(computer, 'MACI')
     mex -LMac -lsndfile -Iinclude src/msndread.c src/utils.c -outdir 'build' ...
+        CFLAGS='\$CFLAGS -std=c99'
+    mex -LMac -lsndfile -Iinclude src/msndblockread.c src/utils.c -outdir 'build' ...
         CFLAGS='\$CFLAGS -std=c99'
 end
