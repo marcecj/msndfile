9b36f25bd1c7258bc4ca2220a81746cc468e1ef4
Marc Joliet <marcec@gmx.de>
2012-03-21 22:46:13 +0100

    start_scons.bat: expect scons to be in PATH.

--- start_scons.bat
+++ start_scons.bat
@@ -1,5 +1,2 @@
-start /b cmd /k C:/Python27/Scripts/scons
-
-REM TODO: It must be possible to find Pythons root directory automatically?
-REM change to wherever SCons is installed on your system.
-doskey scons=C:/Python27/Scripts/scons $*
+REM scons must be in your PATH for this to work
+start /b cmd /k scons
