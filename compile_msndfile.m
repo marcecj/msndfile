7e4b97dc767e9ed2204e1fcba4e6e9dcbf558456
Marc Joliet <marcec@gmx.de>
2012-04-15 16:47:05 +0200

    compile_msndfile.m: add option for defining NOT_HAVE_STDINT_H.

--- compile_msndfile.m
+++ compile_msndfile.m
@@ -1,8 +1,16 @@
 % Simple script for compiling msndfile.
 
+% define whether stdint.h is available
+have_stdint_h = false;
+
 %
 %% compile
 %
+
+win_flags = '';
+if ~have_stdint_h
+    win_flags = '-DNOT_HAVE_STDINT_H';
+end
 
 % needed with GCC >= 4.5, otherwise Matlab crashes
 extra_flags = '-fno-reorder-blocks';
@@ -17,8 +25,8 @@ src2 = [src_dir '/blockread.c ' ...
         src_dir '/audio_files.c'];
 
 if strcmp(computer, 'PCWIN') || strcmp(computer, 'PCWIN64')
-    cmd1 = ['mex -LWin -l''sndfile-1'' -IWin ' src1 ' -outdir ''' out_dir '''']
-    cmd2 = ['mex -LWin -l''sndfile-1'' -IWin ' src2 ' -outdir ''' out_dir '''']
+    cmd1 = ['mex -LWin -l''sndfile-1'' -IWin ' win_flags ' ' src1 ' -outdir ''' out_dir ''''];
+    cmd2 = ['mex -LWin -l''sndfile-1'' -IWin ' win_flags ' ' src2 ' -outdir ''' out_dir ''''];
 elseif strcmp(computer, 'GLNX86') || strcmp(computer, 'GLNXA64')
     cmd1 = ['mex -lsndfile ' src1 ' -outdir ' out_dir ' CFLAGS=''\$CFLAGS''' extra_flags];
     cmd2 = ['mex -lsndfile ' src2 ' -outdir ' out_dir ' CFLAGS=''\$CFLAGS''' extra_flags];
@@ -31,4 +39,13 @@ if ~exist(out_dir, 'dir')
     mkdir('.', out_dir);
 end
 
-eval(cmd1); eval(cmd2);
+disp(cmd1);
+eval(cmd1);
+disp(cmd2);
+eval(cmd2);
+
+if strcmp(computer, 'PCWIN') || strcmp(computer, 'PCWIN64')
+    disp('Copying libsndfile...');
+    copyfile('Win/libsndfile-1.dll', out_dir);
+end
+disp('Done!');
