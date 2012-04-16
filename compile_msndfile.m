72bf73bd9acaa4e4092fb270a049922c052c89f3
Marc Joliet <marcec@gmx.de>
2012-04-16 12:57:18 +0200

    compile_msndfile: support installation, debug build.
    compile_msndfile now supports installation of the resulting build and has
    two additional variables for controlling that.  Furthermore, it now
    supports compiling a debug target.

--- compile_msndfile.m
+++ compile_msndfile.m
@@ -1,7 +1,17 @@
 % Simple script for compiling msndfile.
 
+%
+%% build variables
+%
+
 % define whether stdint.h is available
 have_stdint_h = false;
+% define whether to create a debug build
+do_debug      = false;
+% the installation prefix
+inst_prefix   = '.';
+% the name of the package directory
+pkg_dir       = '+msndfile';
 
 %
 %% compile
@@ -20,7 +30,15 @@ else
 end
 
 src_dir     = 'src/+msndfile';
-out_dir     = 'build/+msndfile';
+
+% the build directory
+if do_debug
+    mex_opts     = [mex_opts ' -g'];
+    build_prefix = 'debug';
+else
+    build_prefix = 'build';
+end
+out_dir = [build_prefix '/' pkg_dir];
 
 src1 = [src_dir '/read.c ' ...
         src_dir '/utils.c ' ...
@@ -47,8 +65,11 @@ end
 eval(cmd1);
 eval(cmd2);
 
+install_dir = [inst_prefix '/' pkg_dir];
+copyfile(out_dir, install_dir);
+
 if strcmp(computer, 'PCWIN') || strcmp(computer, 'PCWIN64')
     disp('Copying libsndfile...');
-    copyfile('Win/libsndfile-1.dll', out_dir);
+    copyfile('Win/libsndfile-1.dll', [inst_prefix '/' pkg_dir]);
 end
 disp('Done!');
